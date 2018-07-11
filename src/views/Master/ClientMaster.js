0173d89df735ed9815ca799570ebbad3a54fd389
liamning <liamning@gmail.com>
2018-07-12 00:00:46 +0800

    datatime picker update

--- src/views/Master/ClientMaster.js
+++ src/views/Master/ClientMaster.js
@@ -33,88 +33,174 @@ export default class ClientMaster extends React.Component {
 
       <div>
         <Row>
-          <Col xs="12" sm="10">
+          <Col xs="12" sm="12" md="12" lg="10">
             <Card>
               <CardHeader>
                 Client
               </CardHeader>
               <CardBody>
-                <form>
-
-                  <FormGroup>
-                    <AsyncSelectField
-                      multi={false}
-                      createAble={true}
-                      name="Code"
-                      {...standardProps}
-
-                      Desc="Code"
-                      tableName="Client"
-                      label={values.Code}
-                      getFormData={getFormData}
-                      afterSave={afterSave}
-
-                    />
-                  </FormGroup>
-
-                  <FormGroup>
-                    <TextField
-                      name="Name"
-                      placeholder="Name"
-                      {...standardProps}
-                    />
-                  </FormGroup>
-
-                  <FormGroup>
-                    <TextField
-                      name="Address"
-                      placeholder="Address"
-                      {...standardProps}
-                    />
-                  </FormGroup>
-
-                  <FormGroup>
-                    <TextField
-                      name="Phone"
-                      placeholder="Phone"
-                      {...standardProps}
-                    />
-                  </FormGroup>
-
-                  <FormGroup>
-                    <TextField
-                      name="Fax"
-                      placeholder="Fax"
-                      {...standardProps}
-                    />
-                  </FormGroup>
-
-                  <FormGroup>
-                    <TextField
-                      name="ContactPerson"
-                      placeholder="ContactPerson"
-                      {...standardProps}
-                    />
-                  </FormGroup>
-                  <FormGroup>
-                    <InputGroup >
-
-                      <DateTimeField
-                        name="RegistrationDate"
-                        placeholder="RegistrationDate"
-                        {...standardProps}
-                      />
-
-                      <InputGroupAddon addonType="append">
-                        <InputGroupText><i className="fa fa-calendar"></i></InputGroupText>
-                      </InputGroupAddon>
-                    </InputGroup>
+                <form autoComplete="off">
 
 
-                  </FormGroup>
+
+                  <Row>
+                    <Col xs="12" md="6">
+
+                      <Row className="form-group">
+                        <Col xs="12" md="4" className="col-form-label">
+                          Email
+                      </Col>
+                        <Col xs="12" md="8">
+                          <AsyncSelectField
+                            multi={false}
+                            createAble={true}
+                            name="Code"
+                            {...standardProps}
+
+                            Desc="Code"
+                            tableName="Client"
+                            label={values.Code}
+                            getFormData={getFormData}
+                            afterSave={afterSave}
+                          />
+                        </Col>
+                      </Row>
+
+                    </Col>
+                  </Row>
 
 
-                  <Button color="success" onClick={onSubmit} type="button" ><i className="fa fa-save"></i>  Submit</Button>
+                  <Row>
+                    <Col xs="12" md="6">
+
+                      <Row className="form-group">
+                        <Col xs="12" md="4" className="col-form-label">
+                          Name
+                      </Col>
+                        <Col xs="12" md="8">
+                          <TextField
+                            name="Name"
+                            placeholder="Name"
+                            {...standardProps}
+                          />
+                        </Col>
+                      </Row>
+
+                    </Col>
+                    <Col xs="12" md="6">
+
+                      <Row className="form-group">
+
+                        <Col xs="12" md="4" className="col-form-label">
+                          Phone
+                      </Col>
+                        <Col xs="12" md="8">
+
+                          <TextField
+                            name="Phone"
+                            placeholder="Phone"
+                            {...standardProps}
+                          />
+                        </Col>
+                      </Row>
+
+                    </Col>
+                  {/* </Row>
+
+
+                  <Row> */}
+                    <Col xs="12" md="6">
+
+                      <Row className="form-group">
+                        <Col xs="12" md="4" className="col-form-label">
+                          Address
+                      </Col>
+                        <Col xs="12" md="8">
+
+                          <TextField
+                            name="Address"
+                            placeholder="Address"
+                            {...standardProps}
+                          />
+                        </Col>
+                      </Row>
+
+                    </Col>
+                    <Col xs="12" md="6">
+
+                      <Row className="form-group">
+
+                        <Col xs="12" md="4" className="col-form-label">
+                          Fax
+                        </Col>
+                        <Col xs="12" md="8">
+
+                          <TextField
+                            name="Fax"
+                            placeholder="Fax"
+                            {...standardProps}
+                          />
+                        </Col>
+                      </Row>
+
+                    </Col>
+                  {/* </Row>
+
+
+                  <Row> */}
+                    <Col xs="12" md="6">
+
+
+                      <Row className="form-group">
+                        <Col xs="12" md="4" className="col-form-label">
+                          Contact Person
+                    </Col>
+                        <Col xs="12" md="8">
+                          <TextField
+                            name="ContactPerson"
+                            placeholder="ContactPerson"
+                            {...standardProps}
+                          />
+                        </Col>
+                      </Row>
+
+
+
+                    </Col>
+                    <Col xs="12" md="6">
+
+
+                      <Row className="form-group">
+
+                        <Col xs="12" md="4" className="col-form-label">
+                          Registration Date
+    
+                    </Col>
+                        <Col xs="12" md="8">
+                          <InputGroup>
+                            <DateTimeField 
+                              name="RegistrationDate"
+                              placeholder="RegistrationDate"
+                              {...standardProps}
+                            />
+                            <InputGroupAddon addonType="append">
+                              <InputGroupText><i className="fa fa-calendar"></i></InputGroupText>
+                            </InputGroupAddon>
+                          </InputGroup>
+
+                        </Col>
+                      </Row>
+
+
+
+                    </Col>
+                  </Row>
+
+
+
+
+
+                  <Button color="success" className="pull-right" onClick={onSubmit} type="button" ><i className="fa fa-save"></i>  Submit</Button>
 
                 </form>
 
